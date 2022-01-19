59bd0bc80573d0af0079359a2246708555a1245a
Nate A <59675817+nargaw@users.noreply.github.com>
2022-01-18 20:21:13 -0500

    update day4

--- Pages 22-28/day4.js
+++ Pages 22-28/day4.js
@@ -49,27 +49,41 @@ let billion = 1_000_000_000;
 //Arithmetic in JavaScript
 // +, -, *, /, % (remainder after division), ** (exponentation)
 //complex mathematical operations through a set of functions and constants Math object
-Math.pow(2, 53)
-Math.round(.7)
-Math.ceil(.4)
-Math.floor(.6)
-Math.abs(-10)
-Math.max(x, y, z)
-Math.min(x, y, z)
-Math.random()
-Math.PI
-Math.E
-Math.sqrt(x)
-Math.pow(y, 1/3)
-Math.sin(z)
-Math.log(10)
-Math.log(100)/Math.LN10
-Math.log(512)/Math.LN2
-Math.exp(3)
+Math.pow(2, 53) //two to the power of 53
+Math.round(.7) //round to nearest integer
+Math.ceil(.4) //round up to an integer
+Math.floor(.6) //round down to an integer
+Math.abs(-10) //absolute value of number
+Math.max(x, y, z) //largest from group
+Math.min(x, y, z) //smallest from group
+Math.random() //pseudo-random number 0 <= x < 1.0
+Math.PI //circumference of a circle / diameter
+Math.E //base of the natural logarithm
+Math.sqrt(x) //square root of x
+Math.pow(y, 1/3) //cube root of y
+Math.sin(z) //sine of z
+Math.log(10) //Natural logarithm
+Math.log(100)/Math.LN10 //Base 10 logarithm
+Math.log(512)/Math.LN2 //Base 2 logarithm
+Math.exp(3) //Math.E cubed
 
 //more function in ES6
-Math.cbrt(27)
-Math.hypot(3,4)
-Math.log10(100)
-Math.log2(1024)
-Math.log1p(x)
+Math.cbrt(27) //cube root
+Math.hypot(3,4) //hypotenuse
+Math.log10(100) //base 10 logarithm
+Math.log2(1024) //base 2 logarithm
+Math.log1p(x) //natural log of (1 +x)
+Math.expm1(x) //Math.exp(x)-1
+Math.sign(x) //negative, positive or zero
+Math.imul(x, y) //optimized multiplication of 32-bit numbers
+Math.clz32(z)  // number of leading zero bits in 32-bit integer
+Math.trunc(3.9) // 3
+Math.fround(y) //Round to nearest 32-bit float number
+Math.sinh(x) //Hyperbolic sine
+Math.asinh(y) //Hyperbolic archsine
+
+//overflow: infinity or -infinity //division by zero
+//underflow: 0 or -0
+//zero divided by zero: NaN (not a number)
+//Infinity divided by Infinity: NaN
+//sqrt of negative number: NaN
