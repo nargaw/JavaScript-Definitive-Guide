932be236cb2e799650cbcf4472a3028b60f43e78
Nate A <59675817+nargaw@users.noreply.github.com>
2022-01-29 23:21:02 -0500

    day15 complete

---
+++ Pages 99-105/day15.js
@@ -0,0 +1,27 @@
+//compound statement combines multiple statements into a single block = A statement Block
+{
+    x = Math.PI;
+    cx = Math.cos(x);
+    console.log("cos(n) = " + cx);
+}
+//enclosed by curly brackets {}
+//doesn't end with a semicolon
+
+//Conditionals
+//1. If -> expression is evaluated, if value is truthy, statement executed
+//         expression is evaluated, if value is falsy, statement not executed
+//parenthesis required around expression
+//If/else 
+//evaluates an expression and executes one of the two pieces of code
+
+//2. Else/If -> if there are multiple pieces of code to choose from
+//else/if is used to evaluate the truthy one
+
+//3. Switch -> when all branches depend on the value of the same expression, more efficent than multiple else/if
+//uses caselable
+//break
+//default
+
+//4.Loops
+//while loops -> repeated execution if value of expression is truthy
+//while(true) - infinite loop
