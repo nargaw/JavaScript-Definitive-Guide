e9f1e7985294275ce9394e13043625f1e52d2c8b
Nate A <59675817+nargaw@users.noreply.github.com>
2022-03-08 05:13:38 -0500

    day 32 complete

--- Pages 212-218/day32.js
+++ Pages 212-218/day32.js
@@ -17,3 +17,34 @@ const sum = (x, y) => x + y;
 const square = x => x * x;
 //use functions with array methods
 let data = [1, 1, 3, 5, 5];
+let mean = reduce(data, sum)/data.length;
+let deviations = map(data, x => x - mean);
+let stddev = Math.sqrt(reduce(map(deviations, square), sum)/(data.length - 1));
+console.log(stddev)//2
+//Higher order functions - a function that operates on functions
+//takes one more functions as arguments and returns a new function
+function mapper(f){
+    return a => map(a, f);
+}
+const increment = x => x + 1;
+const incrementAll = mapper(increment);
+incrementAll([1, 2, 3]) //[2, 3, 4]
+//Partial Application of Functions
+//allow to easily define interesting functions out of functions already defined
+
+//Memoization - caching of previously computed results
+//memoize() - accepts a function as its argument and returns a memoized version of the function
+function memoize(f){
+    const cache = new Map();
+    return function(...args){
+        let key = args.length + args.join("+");
+        if (cache.has(key)){
+            return cache.get(key);
+        } else {
+            let result = f.apply(this, args);
+            cache.set(key, result);
+            return result;
+        }
+    }
+}
+//memoize() function creates a new object to use as the cache and assigns the object to a local variable so it is private to (in clousre of) the returned function
